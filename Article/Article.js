08a42d555ab7b5bf9c5be1bd295f0c6c19b28730
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-02-05 11:31:54 -0800

    Stretch: added close button which make article disappear

--- Article/Article.js
+++ Article/Article.js
@@ -1,21 +1,34 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
-    console.log(domElement);
+  constructor(article) {
+    console.log(article);
     // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
+    this.expandButton = article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
+    //created a reference to the ".closeButton" class.
+    this.closeButton = article.querySelector('.closeButton');
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    console.log(this.expandButton);
-    this.domElement.classList.toggle('article-open');
+    this.article.classList.toggle('article-open');
+    // hide expandButton when article is open
+    this.expandButton.style.display = 'none';
+    // style closeButton same as expandButton
+    this.closeButton.classList.toggle('expandButton');
+    // and show it adding textContent
+    this.closeButton.textContent = 'mark as read';
+    //click event listener for closeButton
+    this.closeButton.addEventListener('click', () => this.markRead());
+  }
+  markRead() {
+    //article is deleted after closeButton is clicked
+    this.article.style.display = 'none';
   }
 }
 
@@ -29,3 +42,37 @@ class Article {
 
 let articles = document.querySelectorAll('.article');
 articles.forEach(article => new Article(article));
+
+
+
+
+
+// //Stretch: user can add his own articles using the form
+// let submitButton = document.querySelector('button');
+
+// submitButton.addEventListener('submit', event => {
+//   // let userArticle = document.querySelector('form');
+//   let userHeader = document.querySelector('.user-header');
+//   let userDate = document.querySelector('.user-date');
+//   let userText = document.querySelector('.user-article');
+
+//   const newArticle = document.createElement('div');
+//   newArticle.className = 'article';
+//   document.querySelector('.articles').appendChild(newArticle);
+//   const newHeader = document.createElement('h2');
+//   newHeader.textContent = userHeader.input;
+//   console.log(userHeader);
+//   newArticle.appendChild(newHeader);
+//   const newDate = document.createElement('p');
+//   newDate.className = 'date';
+//   newDate.textContent = userDate.input;
+//   newArticle.appendChild(newDate);
+//   const newText = document.createElement('p');
+//   newText.textContent = userText.input;
+//   newArticle.appendChild(newText);
+//   const newButton = document.createElement('span');
+//   newButton.className = 'expandButton';
+//   newArticle.appendChild(newButton);
+//   newHeader.textContent = userHeader.nodeValue;
+//   event.preventDefault();
+// });
